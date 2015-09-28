a964d26905f2deb810ff1edab54180b6613fc8ac
Juan Sarria <jdsarr@gmail.com>
2015-09-28 14:00:37 -0700

    Sept 28, 2015 1:59PM
    -Added Hadoop JARs to classpath and lib folder
    -Turned EulerEstimator into generic mapreduce class without reducer

--- CMPT732-A2A/src/EulerEstimator.java
+++ CMPT732-A2A/src/EulerEstimator.java
@@ -8,16 +8,15 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.NullOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
 public class EulerEstimator extends Configured implements Tool {
 
-	   public static class MaxWikiMapper
+	   public static class EulerMapper
 	    extends Mapper<LongWritable, Text, Text, LongWritable>{
 	 
 	        private final static LongWritable one = new LongWritable();
@@ -50,52 +49,25 @@ public class EulerEstimator extends Configured implements Tool {
 	        }
 	    }
 	   
-	   
-	   
-	   public static class MaxWikiReducer
-	    extends Reducer<Text, LongWritable, Text, LongWritable> {
-		   private LongWritable result = new LongWritable();
-		   
-		   @Override
-	        public void reduce(Text key, Iterable<LongWritable> values,
-	                           Context context) throws IOException, InterruptedException {
-	            long max = 0;
-	            
-	            //Find the maximum value for a specific key
-	            for (LongWritable val : values) {
-	                if(val.get()>max)
-	                	max = val.get();
-	            }
-	            
-	            result.set(max);
-	            context.write(key, result);
-	        }   
-	   }
-	   
 
 	    
 	    public static void main(String[] args) throws Exception {
-	        int res = ToolRunner.run(new Configuration(), new WikipediaPopular(), args);
+	        int res = ToolRunner.run(new Configuration(), new EulerEstimator(), args);
 	        System.exit(res);
 	    }
 	 
 	    @Override
 	    public int run(String[] args) throws Exception {
 	        Configuration conf = this.getConf();
-	        Job job = Job.getInstance(conf, "wikipedia popular");
-	        job.setJarByClass(WikipediaPopular.class);
+	        Job job = Job.getInstance(conf, "EulerEstimator");
+	        job.setJarByClass(EulerEstimator.class);
 	 
 	        job.setInputFormatClass(TextInputFormat.class);
 	 
-	        job.setMapperClass(MaxWikiMapper.class);
-	        job.setCombinerClass(MaxWikiReducer.class);
-	        job.setReducerClass(MaxWikiReducer.class);
-	 
-	        job.setOutputKeyClass(Text.class);
-	        job.setOutputValueClass(LongWritable.class);
-	        job.setOutputFormatClass(TextOutputFormat.class);
+	        job.setMapperClass(EulerMapper.class);
+
+	        job.setOutputFormatClass(NullOutputFormat.class);
 	        TextInputFormat.addInputPath(job, new Path(args[0]));
-	        TextOutputFormat.setOutputPath(job, new Path(args[1]));
 	 
 	        return job.waitForCompletion(true) ? 0 : 1;
 	    }
