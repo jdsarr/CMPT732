08d5e93f79be2efee83dddf9702c0537c8c45651
Juan Sarria <jdsarr@gmail.com>
2015-09-30 15:12:00 -0700

    Sept 30, 2015 3:11 PM
    -Changed classpath

--- CMPT732-A2A/src/LoadLogs.java
+++ CMPT732-A2A/src/LoadLogs.java
@@ -6,6 +6,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.regex.Pattern;
+
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.client.Connection;
