b10d49c86b22cd72a0f96c9b1b26d3e3681c6654
jdsarr <jdsarr@gmail.com>
2015-09-29 23:48:10 -0700

    Sept 29, 2015
    -Worked on get_put function in LoadLogs

--- CMPT732-A2A/src/LoadLogs.java
+++ CMPT732-A2A/src/LoadLogs.java
@@ -1,8 +1,15 @@
 
+
+import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.ConnectionFactory;
 import org.apache.hadoop.hbase.HBaseConfiguration;
-import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.Table;
+import org.apache.hadoop.hbase.util.Bytes;
+
 
 
 
@@ -12,9 +19,26 @@ import org.apache.hadoop.hbase.client.Put;
 
 public class LoadLogs {
 	
+	static private Configuration conf;
+	static private Connection connection;
+	static private Table table;
+	
     public static Put get_put(String line) throws Exception {
     	
-    	Configuration conf = HBaseConfiguration.create();
+    	byte[] rowkey = DigestUtils.md5(line);
+    	Put put = new Put(rowkey);
+    	put.addColumn(Bytes.toBytes("raw"), Bytes.toBytes("line"), Bytes.toBytes(line));
+    	
+    	table.put(put);
+    	
+    	return put;
+    }
+    
+    public static void main(String[] args) throws Exception {
+    	
+    	conf = HBaseConfiguration.create();
+    	connection = ConnectionFactory.createConnection(conf);
+    	table = connection.getTable(TableName.valueOf(args[0]));
     	
     	
     }
