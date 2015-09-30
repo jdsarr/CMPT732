334c2a4a97c4403fb30bb77ba61392c9b167eb38
jdsarr <jdsarr@gmail.com>
2015-09-29 19:57:25 -0700

    Sept 29, 2015 7:57 PM
    -Added HBase JARs

--- CMPT732-A2A/src/LoadLogs.java
+++ CMPT732-A2A/src/LoadLogs.java
@@ -1,6 +1,11 @@
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.client.HTable;
 import org.apache.hadoop.hbase.client.Put;
+
+
+
 
 
 
@@ -9,7 +14,9 @@ public class LoadLogs {
 	
     public static Put get_put(String line) throws Exception {
     	
-    	Configuration conf = HbaseConfiguration.create();
+    	Configuration conf = HBaseConfiguration.create();
+    	
+    	
     }
 
 }
