22a4a303a137307922a4006a0763f9257ee2d06e
jdsarr <jdsarr@gmail.com>
2015-10-06 01:01:30 -0700

    Oct 6, 2015 1:01 AM
    -Working on CorrelateLogs

--- CMPT732-A2B/src/CorrelateLogs.java
+++ CMPT732-A2B/src/CorrelateLogs.java
@@ -2,22 +2,25 @@ import java.io.IOException;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hbase.CellUtil;
-import org.apache.hadoop.hbase.client.Mutation;
+//import org.apache.hadoop.hbase.client.Mutation;
 import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.client.Scan;
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
 import org.apache.hadoop.hbase.mapreduce.TableMapReduceUtil;
 import org.apache.hadoop.hbase.mapreduce.TableMapper;
-import org.apache.hadoop.hbase.mapreduce.TableOutputFormat;
+//import org.apache.hadoop.hbase.mapreduce.TableOutputFormat;
 import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.chain.ChainReducer;
-import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+//import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
@@ -25,10 +28,10 @@ import org.apache.hadoop.util.ToolRunner;
 public class CorrelateLogs extends Configured implements Tool {
 
 	
-	public static class CorrelateLogsMapper extends TableMapper<Text, LongPairWritable>{
+	public static class CorrelateMapper extends TableMapper<Text, LongPairWritable>{
 		
-		private Text host;
-		private LongPairWritable pair;
+		private Text host = new Text();
+		private LongPairWritable pair = new LongPairWritable();
 		
 		public void map(ImmutableBytesWritable key, Result value, Context context) throws IOException, InterruptedException {
 			
@@ -40,13 +43,13 @@ public class CorrelateLogs extends Configured implements Tool {
 										Bytes.toBytes("struct"),Bytes.toBytes("bytes"))));
 			
 			
-			
 			host.set(hostString);
 			pair.set(1, bytesLong);
 			context.write(host, pair);
 		}
 	}
 
+	
 	
 	public static class CorrelateReducer 
 	extends Reducer<Text, LongPairWritable, LongWritable, LongWritable> {
@@ -75,8 +78,9 @@ public class CorrelateLogs extends Configured implements Tool {
 	}
 	
 	
+	
 	public static class ChainMapper
-	extends Mapper<LongWritable,LongWritable, LongWritable, LongWritable> {
+	extends Mapper<LongWritable,LongWritable, Text, DoubleWritable> {
 		
 		long n;
 		long Sx;
@@ -93,32 +97,37 @@ public class CorrelateLogs extends Configured implements Tool {
 		}
 		
         public void map(LongWritable key, LongWritable value, Context context
-                ) throws IOException, InterruptedException {
-        	
+                ) throws IOException, InterruptedException { 	
         	n++;
         	Sx+=(key.get());
         	Sx2+=(key.get())^2;
         	Sy+=(value.get());
         	Sy2+=(value.get())^2;
-        	Sxy+=(key.get()*value.get());
-			
+        	Sxy+=(key.get()*value.get());	
 		}
         
-		protected void cleanup(Context context) throws IOException, InterruptedException {
-			
-			 r = ((double)(n*Sxy-Sx*Sy))/
+		protected void cleanup(Context context) throws IOException, InterruptedException {	
+		
+			r = ((double)(n*Sxy-Sx*Sy))/
 					 (Math.sqrt(n*Sx2-(Sx)^2)*Math.sqrt(n*Sy2-(Sy)^2));
-			 r2 = r*r;
+			r2 = r*r;
 			 
-			 System.out.println("n = " + n);
-			 System.out.println("Sx = " + Sx);
-			 System.out.println("Sx2 = " + Sx2);
-			 System.out.println("Sy = " + Sy);
-			 System.out.println("Sy2 = " + Sy2);
-			 System.out.println("Sxy = " + Sxy);
-			 System.out.println("r = " + r);
-			 System.out.println("r2 = " + r2);
-			 
+			System.out.println("n = " + n);
+			context.write(new Text("n"), new DoubleWritable((double)n));
+			System.out.println("Sx = " + Sx);
+			context.write(new Text("Sx"), new DoubleWritable((double)Sx));
+			System.out.println("Sx2 = " + Sx2);
+			context.write(new Text("Sx2"), new DoubleWritable((double)Sx2));
+			System.out.println("Sy = " + Sy);
+			context.write(new Text("Sy"), new DoubleWritable((double)Sy));
+			System.out.println("Sy2 = " + Sy2);
+			context.write(new Text("Sy2"), new DoubleWritable((double)Sy2));
+			System.out.println("Sxy = " + Sxy);
+			context.write(new Text("Sxy"), new DoubleWritable((double)Sxy));
+			System.out.println("r = " + r);
+			context.write(new Text("r"), new DoubleWritable(r));
+			System.out.println("r2 = " + r2);		 
+			context.write(new Text("r2"), new DoubleWritable(r2));
 		}
         
 	}
@@ -127,33 +136,40 @@ public class CorrelateLogs extends Configured implements Tool {
 	
 	
     public static void main(String[] args) throws Exception {
-        int res = ToolRunner.run(new Configuration(), new LoadLogsMR(), args);
+        int res = ToolRunner.run(new Configuration(), new CorrelateLogs(), args);
         System.exit(res);
     }
  
+    
     
 	@Override
 	public int run(String[] args) throws Exception {
         Configuration conf = this.getConf();
         Job job = Job.getInstance(conf, "Correlate Logs");
-        job.setJarByClass(LoadLogsMR.class);
-       
-        job.setInputFormatClass(TextInputFormat.class);
+        job.setJarByClass(CorrelateLogs.class);
         
         Scan scan = new Scan();
         scan.addColumn(Bytes.toBytes("struct"),Bytes.toBytes("host"));
         scan.addColumn(Bytes.toBytes("struct"),Bytes.toBytes("bytes"));
         
         TableMapReduceUtil.addDependencyJars(job);
-        TableMapReduceUtil.initTableMapperJob(args[1],scan,CorrelateLogsMapper.class, Text.class, LongPairWritable.class,job);
-
-        
+        TableMapReduceUtil.initTableMapperJob(args[0],scan,CorrelateMapper.class, Text.class, LongPairWritable.class,job);     
         job.setMapOutputKeyClass(Text.class);
         job.setMapOutputValueClass(LongPairWritable.class);
         
         job.setNumReduceTasks(1);	
         ChainReducer.setReducer(job, CorrelateReducer.class, 
         		Text.class, LongPairWritable.class, LongWritable.class, LongWritable.class, new Configuration(false));
+        ChainReducer.addMapper(job, ChainMapper.class, 
+        		LongWritable.class, LongWritable.class, LongWritable.class, LongWritable.class, new Configuration(false));
+        
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(DoubleWritable.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+        
+        if(args.length == 2)
+        	TextOutputFormat.setOutputPath(job, new Path(args[1]));
+        
         
         return job.waitForCompletion(true) ? 0 : 1;
 	}
